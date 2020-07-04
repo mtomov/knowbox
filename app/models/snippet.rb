1a0a72ec8ec345e75c21fa67edab78f415e81bc7
Martin Tomov <mtomov@outlook.com>
2020-07-04 10:05:31 +0100

    🔧  Configure PgSearch to highlight larger areas

--- app/models/snippet.rb
+++ app/models/snippet.rb
@@ -1,7 +1,16 @@
 class Snippet < ApplicationRecord
   include PgSearch::Model
   pg_search_scope :search, against: :text, using: {
-    tsearch: {dictionary: "english", any_word: true, prefix: true}
+    tsearch: {dictionary: "english", any_word: true, prefix: true, highlight: {
+      StartSel: "<b>",
+      StopSel: "</b>",
+      MaxWords: 123,
+      MinWords: 456,
+      ShortWord: 4,
+      HighlightAll: true,
+      MaxFragments: 3,
+      FragmentDelimiter: "&hellip;"
+    }}
   }
 
   belongs_to :author, class_name: "User", required: false
