c812c0d178d4a50dfd572fcbed0c8794f28f331a
Martin Tomov <mtomov@outlook.com>
2020-07-06 14:01:46 +0100

    🗑  Remove unused Sentry
      - key is not valid

--- config/initializers/sentry.rb
+++
@@ -1,4 +0,0 @@
-Raven.configure do |config|
-  config.dsn = "https://77666100479f479587624ec6881292bf:e2d9813e8fc6421396c71afc9c7d7f47@sentry.io/1818896"
-  config.environments = %w[staging production]
-end
